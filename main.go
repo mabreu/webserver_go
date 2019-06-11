813330a6646aa5638bcc0f39527411191a88f7ca
Marco Antonio Abreu <falecomabreu@gmail.com>
2019-06-11 17:47:49 -0300

    versão final, publicada no vídeo

--- main.go
+++ main.go
@@ -2,40 +2,71 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 
 	"./mywebserver"
 )
 
-// Somar soma 2 numeros
-func somar(req *mywebserver.Request) {
-	fmt.Fprintln(req.GetResponseWriter(), "A soma é")
-	req.Done()
-}
-
-// Subtrair subtrai 2 numeros
-func subtrair(req *mywebserver.Request) {
-	fmt.Fprintln(req.GetResponseWriter(), "A substração é")
-	req.Done()
-}
-
-// potencia_quadrado subtrai 2 numeros
-func potenciaQuadrado(req *mywebserver.Request) {
-	fmt.Fprintln(req.GetResponseWriter(), "A potencia quadrado é")
-	req.Done()
-}
-
-// Subtrair subtrai 2 numeros
-func potenciaCubo(req *mywebserver.Request) {
-	fmt.Fprintln(req.GetResponseWriter(), "A potencia cubo é")
-	req.Done()
-}
+var myws mywebserver.MyWebServer
+var versao = "1.2.3"
 
 func main() {
-	var meuws = mywebserver.MyWebServer{}
-	meuws.New(8080)
-	meuws.AddHandler("somar", somar)
-	meuws.AddHandler("subtrair", subtrair)
-	meuws.AddHandler("power/square", potenciaQuadrado)
-	meuws.AddHandler("power/cube", potenciaCubo)
-	meuws.Start()
+	myws = mywebserver.MyWebServer{}
+	myws.New(8080)
+	myws.AddHandler("somar", somar)
+	myws.AddHandler("potencia/quadrado", potenciaQuadrado)
+	myws.AddHandler("versao", versaoHandle)
+	myws.Start()
+}
+
+// Somar executa a soma de dois números
+func somar(req *mywebserver.MyRequest) {
+	x := req.HTTPRequest.FormValue("x")
+	y := req.HTTPRequest.FormValue("y")
+
+	if x == "" || y == "" {
+		fmt.Fprintln(req.HTTPResponse, "Falta(m) parâmetro(s).")
+	} else {
+		fx, err := strconv.ParseFloat(x, 32)
+
+		if err != nil {
+			fmt.Fprintln(req.HTTPResponse, "Parâmetro X inválido.")
+		}
+
+		fy, err := strconv.ParseFloat(y, 32)
+
+		if err != nil {
+			fmt.Fprintln(req.HTTPResponse, "Parâmetro Y inválido.")
+		}
+
+		fmt.Fprintf(req.HTTPResponse, "A soma de %10.5f e %10.5f é %10.5f", fx, fy, fx+fy)
+	}
+
+	req.Done()
+}
+
+// potenciaQuadrado eleva ao quadrado o valor informado
+func potenciaQuadrado(req *mywebserver.MyRequest) {
+	x := req.HTTPRequest.FormValue("x")
+
+	if x == "" {
+		fmt.Fprintln(req.HTTPResponse, "Falta parâmetro.")
+	} else {
+		fx, err := strconv.ParseFloat(x, 32)
+
+		if err != nil {
+			fmt.Fprintln(req.HTTPResponse, "Parâmetro X inválido.")
+		}
+
+		fmt.Fprintf(req.HTTPResponse, "O quadrado de %10.5f é %10.5f", fx, math.Pow(fx, 2))
+	}
+
+	req.Done()
+}
+
+// versaoHandle informa a versão do web service
+func versaoHandle(req *mywebserver.MyRequest) {
+	fmt.Fprintln(req.HTTPResponse, "A versão da minha API é", versao)
+	req.Done()
 }
--- mywebserver/mywebserver.go
+++ mywebserver/mywebserver.go
@@ -7,75 +7,54 @@ import (
 	"strings"
 )
 
-// MyWebServer é uma estrutura (classe) para representar o meu servidor web
+// MyWebServer é a estrutura de dados básica do nosso web server
 type MyWebServer struct {
-	handlers map[string]func(req *Request)
-	port     int
-	version  string
+	handlers map[string]func(req *MyRequest)
+	Port     int
 }
 
-// New inicaliza o servidor Web
+// New inicializa a estrutura MyWebServer
 func (ws *MyWebServer) New(port int) {
-	ws.version = "1.0.0" // Versão de 02/06/2019
-
 	if port == 0 {
 		port = 8080
 	}
 
-	ws.SetPort(port)
-	ws.handlers = make(map[string]func(req *Request))
+	ws.Port = port
+	ws.handlers = make(map[string]func(req *MyRequest))
 	ws.AddHandler("/", ws.rootHandle)
 }
 
-// GetVersion retorna a versão do servidor web
-func (ws *MyWebServer) GetVersion() string {
-	return ws.version
-}
-
-// SetPort define a porta que será utilizada no servidor web
-func (ws *MyWebServer) SetPort(port int) {
-	ws.port = port
-}
-
-// GetPort retorna a porta definida
-func (ws *MyWebServer) GetPort() int {
-	return ws.port
-}
-
-// AddHandler adiciona a chamada na lista de chamadas possíveis
-func (ws *MyWebServer) AddHandler(name string, fun func(req *Request)) {
-	fmt.Println("AddHandler", name)
+// AddHandler adiciona um novo serviço ao nosso Servidor
+func (ws *MyWebServer) AddHandler(name string, fun func(req *MyRequest)) {
+	fmt.Println("AddHandler: ", name)
 
 	for len(name) > 0 && name[0:1] == "/" {
 		name = name[1:]
 	}
 
-	fmt.Println("AddHandler", name)
 	name = "/" + name
 	ws.handlers[name] = fun
 	http.HandleFunc(name, ws.listener)
 }
 
-// Start inicia o servidor
+// Start inicia o servidor web MyWebServer
 func (ws *MyWebServer) Start() {
-	var port = ":" + strconv.Itoa(ws.GetPort())
-	fmt.Println("Starting MyWebServer with port", port, "...")
-	http.ListenAndServe(port, nil)
+	fmt.Println("Starting MyWebServer with port", ws.Port, "...")
+	http.ListenAndServe(":"+strconv.Itoa(ws.Port), nil)
 }
 
-func (ws *MyWebServer) rootHandle(req *Request) {
-	fmt.Fprintln(req.GetResponseWriter(), "MyWebServer is running at", ws.GetPort())
+func (ws *MyWebServer) rootHandle(req *MyRequest) {
+	fmt.Fprintln(req.HTTPResponse, "MyWebServer is running at", ws.Port)
 	req.Done()
 }
 
-// Listener recebe as chamadas e distribui de acordo com o nome
 func (ws *MyWebServer) listener(hrw http.ResponseWriter, hreq *http.Request) {
-	fmt.Println("URI: ", hreq.RequestURI)
+	fmt.Println("URI:", hreq.RequestURI)
 	uri := hreq.RequestURI
 	pos := strings.Index(uri, "?")
 
 	if pos == 0 {
-		http.Error(hrw, "MyWebServer: No Service specified.", http.StatusNotImplemented)
+		http.Error(hrw, "MyWebServer: No service Specified.", http.StatusNotImplemented)
 		return
 	}
 
@@ -84,7 +63,6 @@ func (ws *MyWebServer) listener(hrw http.ResponseWriter, hreq *http.Request) {
 	}
 
 	name := hreq.RequestURI[0:pos]
-	fmt.Println("Name: ", name)
 	fun, found := ws.handlers[name]
 
 	if !found || fun == nil {
@@ -92,8 +70,8 @@ func (ws *MyWebServer) listener(hrw http.ResponseWriter, hreq *http.Request) {
 		return
 	}
 
-	var req = Request{}
-	req.New(hrw, hreq)
-	go fun(&req)
-	req.Wait()
+	var myreq = MyRequest{}
+	myreq.New(hrw, hreq)
+	go fun(&myreq)
+	myreq.Wait()
 }
